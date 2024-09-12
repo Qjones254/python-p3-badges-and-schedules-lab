4737a70ffa5a80be8e69745478681668161d7bb3
quincy mwangi <mwangiquincy77@gmail.com>
2024-09-12 12:46:08 +0300

    complete badges and schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,37 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+ 
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+
+print(badge_maker("Arel")) 
+
+names = ["Arel", "Jimmy", "Quincy","Kelvin"]
+badges = batch_badge_creator(names)
+print(badges)  
+
 
 def assign_rooms(names):
-    return None
+    rooms = list(range(1,9))
+
+    room_assignments =[]
+    for i in range(len(names)):
+        name = names[i]
+        room = rooms[i]
+        room_assignments.append(f"Hello, {name}! You'll be assigned to room {room}!")
+    return room_assignments
+
+speakers = ["Alice", "Jimmy", "Charlie", "David", "Quincy", "Arel", "Kelvin", "Hannah"]
+assignments = assign_rooms(speakers)
+for assignment in assignments:
+    print(assignment)
 
 def printer(names):
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print (badge)
+    assignments = assign_rooms(names)
+    for assignment in assignments:
+        print (assignment)
     return None
